e4fe4fde07586094f557256e23a92a5f8446df9a
Hoang Van Viet Anh <hoangvanvietanh@gmail.com>
2020-09-11 06:08:13 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,51 @@
+var data = [{
+    "topic": "Số",
+    "words":[
+        {
+            "word":"A",
+            "meaning":"Chữ A",
+            "sound":"mp1",
+            "writing":"...."
+        },{
+            "word":"B",
+            "meaning":"Chữ B",
+            "sound":"mp2",
+            "writing":"...."
+        },{
+            "word":"C",
+            "meaning":"Chữ C",
+            "sound":"mp3",
+            "writing":"...."
+        },{
+            "word":"D",
+            "meaning":"Chữ D",
+            "sound":"mp4",
+            "writing":"...."
+        },
+    ]
+},{
+    "topic": "Đất nước",
+    "words":[
+        {
+            "word":"E",
+            "meaning":"Chữ E",
+            "sound":".....",
+            "writing":"...."
+        },{
+            "word":"F",
+            "meaning":"Chữ F",
+            "sound":".....",
+            "writing":"...."
+        },{
+            "word":"G",
+            "meaning":"Chữ G",
+            "sound":".....",
+            "writing":"...."
+        },{
+            "word":"H",
+            "meaning":"Chữ H",
+            "sound":".....",
+            "writing":"...."
+        },
+    ]
+}]
